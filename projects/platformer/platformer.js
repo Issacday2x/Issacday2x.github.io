5d6d9c683e32c4836a5083f1db09b22e1bcfd53b
Issacday2x <Issac.day@carvercollegiate.org>
2024-10-24 17:05:14 +0000

    add bouncing box

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -28,7 +28,7 @@ $(function () {
      * Comment the function call out to remove the grid
      */
 
-    // drawGrid();
+    drawGrid();
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE BELOW THIS POINT///////////
@@ -38,26 +38,27 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
-    
+    createPlatform(100,600,100,20)
+    createPlatform(200,350,00,20)
+    createPlatform(400,300,200,20)
+    createPlatform(640,250,200,20)
+    createPlatform(300,400,200,20)
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
-    
+    createCollectable("grace",500,300,2,1)
+    createCollectable("steve",500,300,2,1)
+    createCollectable("kennedi",500,300,2,1)
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
+    createCannon("right", 600,1000); // cannon on right wall, 600px down, shooting twice per second
+    createCannon("left",500,1000); // cannon on left wall, 500px up, shooting once per second
+    createCannon("right",500,1000); //cannon on left wall, 600px down, shooting three times per second
 
     
     /////////////////////////////////////////////////
